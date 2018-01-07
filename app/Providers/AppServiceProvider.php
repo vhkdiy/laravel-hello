7ef21e4e8a344bec7301d99712c84a7e8b7c0066
vhkdiy <404924175@qq.com>
2018-01-07 23:53:04 +0800

    add send message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('zh');
+        Schema::defaultStringLength(191);
     }
 
     /**
