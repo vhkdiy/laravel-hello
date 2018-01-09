77488d33b1401cfe855ed35ab7274deb91cf588b
vhkdiy <404924175@qq.com>
2018-01-09 23:51:33 +0800

    followers

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,14 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
 }
